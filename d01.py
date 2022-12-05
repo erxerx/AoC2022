d3ce3865ef7001d3a4c10210fb123ebdad57e3b5
Ervin Hülp <ervin.hylp@gmail.com>
2022-12-05 19:31:23 +0200

    Day 1

---
+++ d01.py
@@ -0,0 +1,13 @@
+with open('d01.in', 'r') as f:
+    content = f.read()
+d1 = content.split('\n\n')
+data = [x.replace('\n', ' ').split(' ') for x in d1]
+ss = []
+for x in data:
+    s = 0
+    for y in x:
+        s += int(y)
+    ss.append(s)
+print('Most:', max(ss))
+ss.sort(reverse=True)
+print('Most3:', ss[0] + ss[1] + ss[2])
