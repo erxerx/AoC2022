f6c4c2890b13529c8b62ed91c836ca446033045c
Ervin Hülp <ervin.hylp@gmail.com>
2022-12-10 20:22:25 +0200

    Day 2

---
+++ d02.py
@@ -0,0 +1,19 @@
+with open('d02.in', 'r') as f:
+    d = f.readlines()
+data = [x.replace('\n', '') for x in d]
+s1 = s2 = 0
+points1 = {
+    'A X': 4, 'A Y': 8, 'A Z': 3,
+    'B X': 1, 'B Y': 5, 'B Z': 9,
+    'C X': 7, 'C Y': 2, 'C Z': 6,
+}
+points2 = {
+    'A X': 3, 'A Y': 4, 'A Z': 8,
+    'B X': 1, 'B Y': 5, 'B Z': 9,
+    'C X': 2, 'C Y': 6, 'C Z': 7,
+}
+for x in data:
+    s1 += points1[x]
+    s2 += points2[x]
+print('Total1:', s1)
+print('Total1:', s2)
